0ec1b374de105186127ba81ceb86d8ae029f09eb
Islam Temirgaliev <islam2004tr@mail.ru>
2023-11-17 15:18:42 +0600

    commit router ardyino

--- src/mainRouter.ts
+++ src/mainRouter.ts
@@ -1,10 +1,12 @@
 
 import battryRouter from "../src/routes_arduino/Battery/battery.route"
+import solorPanels from "../src/routes_arduino/solar_panels/solar.route"
 import { Router } from 'express'
 
 const mainRouter = Router()
 
 mainRouter.use("/battery", battryRouter)
+mainRouter.use("/SolarP", solorPanels)
 
 
 
--- src/routes_arduino/Battery/battery.controller.ts
+++ src/routes_arduino/Battery/battery.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express'
-import {awsConnection} from "../../connection"
+// import {awsConnection} from "../../connection"
 import BadRequestError from '../../errors/bad-request';
 import { StatusCodes } from 'http-status-codes';
 
@@ -7,16 +7,16 @@ import { StatusCodes } from 'http-status-codes';
 export async function postBattery(req: Request, res: Response) {
 const {data} = req.body;
 
-const params = {
-    Bucket: 'YOUR_BUCKET_NAME',
-    Key: 'YOUR_OBJECT_KEY',
-    Body: data, 
-    ContentType: 'text/plain' 
-  };
+// const params = {
+//     Bucket: 'YOUR_BUCKET_NAME',
+//     Key: 'YOUR_OBJECT_KEY',
+//     Body: data, 
+//     ContentType: 'text/plain' 
+//   };
   
 try{
-    const result = await awsConnection.putObject(params).promise();
-
+    // const result = await awsConnection.putObject(params).promise();
+res.send(data)
     return res.status(StatusCodes.OK).json({ message: 'данныйе успешно отправлены' })
 
 }catch{
