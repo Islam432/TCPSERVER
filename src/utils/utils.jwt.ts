20e110f78e04ed56121e90179c121ae5686fdca9
Islam Temirgaliev <islam2004tr@mail.ru>
2023-12-04 12:51:30 +0600

    jwt

--- src/utils/utils.jwt.ts
+++ src/utils/utils.jwt.ts
@@ -5,17 +5,17 @@ const PRIVATE_KEY = fs.readFileSync(`${__dirname}/../keys/id_rsa_priv.pem`, 'utf
 
 const user = {
   email: 'user@.com',
-  role_name: 'admin',
-   id: 1,
+  /* role_name: 'admin',
+   id: 1, */
 };
 
-function issueJWT(user: { email: string; role_name: string; id: number }) {
-  const { email, role_name, id } = user
+function issueJWT(user: { email: string; /* role_name: string; */ /* id: number */ }) {
+  const { email, /* role_name, id  */} = user
   const payload = {
     sub: email,
     iat: Date.now(),
-    role_name,
-    id,
+   /*  role_name,
+    id, */
   }
 
   const signedJWT = jwt.sign(payload, PRIVATE_KEY, { expiresIn: '30d', algorithm: 'RS256' })
