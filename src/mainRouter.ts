10ee716674e28159823f3d8d7585525be466aba0
Islam Temirgaliev <islam2004tr@mail.ru>
2023-12-04 12:51:07 +0600

    das

--- src/mainRouter.ts
+++ src/mainRouter.ts
@@ -1,10 +1,11 @@
-
+import auth from '../src/auth/auth.route'
 import battryRouter from "../src/routes_arduino/Battery/battery.route"
 import solorPanels from "../src/routes_arduino/solar_panels/solar.route"
 import { Router } from 'express'
 
 const mainRouter = Router()
 
+mainRouter.use("/auth", auth)
 mainRouter.use("/battery", battryRouter)
 mainRouter.use("/SolarP", solorPanels)
 
