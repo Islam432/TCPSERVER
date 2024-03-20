fd90d1bd555ae65459bb56800f154ee167007a65
Ilham Temirgaliev <ilham2004t@gmail.com>
2024-03-20 13:06:20 +0600

    valid

--- src/routes_arduino/Battery/battery.controller.ts
+++ src/routes_arduino/Battery/battery.controller.ts
@@ -1,35 +1,57 @@
-import { Request, Response } from 'express'
-// import {awsConnection} from "../../connection"
+import { z } from "zod";
+import { Request, Response } from 'express';
 import BadRequestError from '../../errors/bad-request';
 import { StatusCodes } from 'http-status-codes';
 import winston from 'winston';
 
-export async function postBattery(req: Request, res: Response) {
-const {data} = req.body;
-
-try {
-   
-    const logger = winston.createLogger({
-      transports: [
-        new winston.transports.Console(), 
+// Создаем логгер
+const logger = winston.createLogger({
+    transports: [
+        new winston.transports.Console(),
         new winston.transports.File({ filename: 'server.log' }),
-      ],
-      format: winston.format.combine(
-        winston.format.timestamp(), 
-        winston.format.json() 
-      ),
-    });
+    ],
+    format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.json()
+    ),
+});
 
-    const dataString = JSON.stringify(req.body);
-    logger.info(`данные успешно получены: ${dataString}`);
+// Определяем схему данных с использованием Zod
+const batterySchema = z.object({
+    id: z.number(),
+    b_v: z.number().nonnegative(),
+    sp_v: z.number().nonnegative(),
+    l_v: z.number().nonnegative(),
+    b_c: z.number().nonnegative(),
+    sp_c: z.number().nonnegative(),
+    b_disch_c: z.number().nonnegative(),
+    b_temp: z.number(),
+    b_remain: z.number().min(0).max(100),
+});
 
+export async function postBattery(req: Request, res: Response) {
+    const  data  = req.body;
     console.log(data);
+    try {
+        if (!data) {
+            throw new BadRequestError('Отсутствуют данные в запросе');
+        }
+        
+        const batteryData = batterySchema.parse(data);
+        console.log(batteryData);
 
-    
-    res.status(StatusCodes.OK).json({ result: data });
-  } catch (error) {
+        const dataString = JSON.stringify(req.body);
+        logger.info(`Данные успешно получены: ${dataString}`);
 
-    throw new BadRequestError('данные не отправлены');
-  }
+        console.log(batteryData);
 
+        res.status(StatusCodes.OK).json({ result: data });
+    } catch (error) {
+        // Логируем ошибку
+        const errorMessage = error instanceof BadRequestError ? error.message : 'Внутренняя ошибка сервера';
+        logger.error(`Ошибка при обработке запроса: ${errorMessage}`);
+
+        // Возвращаем клиенту ошибку
+        res.status(StatusCodes.BAD_REQUEST).json({ error: errorMessage });
+    }
 }
